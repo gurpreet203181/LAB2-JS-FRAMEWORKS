b2def4c9a242d4518cf82198059e8adbd3f28695
Gurpreet Singh <gs203181@gmail.com>
2025-02-20 16:09:35 -0500

    index change

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // Importing functions 
 import { greet, celsiusToFahrenheit, factorial, reverseString } from './utilities.js';
 
-greet('John', 'Doe');  // Expected Output: Welcome John Doe to G@I College
+greet('Gurpreet', 'Singh');  // Expected Output: Welcome John Doe to G@I College
 
-const tempCelsius = 25;
+const tempCelsius = 22;
 console.log(`${tempCelsius}°C = ${celsiusToFahrenheit(tempCelsius)}°F`);
 
-const num = 5;
+const num = 2;
 console.log(`Factorial of ${num} = ${factorial(num)}`);
 
-const text = "Hello World";
+const text = " Welcome";
 console.log(`Reversed Text: ${reverseString(text)}`);
